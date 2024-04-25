f68b260f941669de3402544b946de7c1da110fad
Adrià Martí Comas <adriamarticomas@gmail.com>
2024-04-25 10:23:35 +0200

    Update PortfolioController.java
    Controller done

--- src/main/java/com/serverFile/controller/PortfolioController.java
+++ src/main/java/com/serverFile/controller/PortfolioController.java
@@ -1,33 +1,106 @@
 package com.serverFile.controller;
 
 import com.serverFile.model.domain.Portfolio;
+import com.serverFile.model.domain.Project;
+import com.serverFile.model.domain.Title;
 import com.serverFile.model.service.PortfolioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/portfolios")
+@RequestMapping("/api/portfolio")
 public class PortfolioController {
 
     @Autowired
     private PortfolioServiceImpl portfolioService;
 
-    //TODO
-    //get all projects from a portfolio - public GET
-    @GetMapping("/{owner}/projects")
-    public Portfolio getPortfolioProjects(String owner){
-        return portfolioService.getPortfolio(owner);
+    //Public methods
+
+    //GET FULL PORTFOLIO
+    @GetMapping("/{portfolioName}")
+    public ResponseEntity<Portfolio> getPortfolio(@PathVariable String portfolioName) {
+        return ResponseEntity.ok(portfolioService.getPortfolio(portfolioName));
     }
 
-    //Post new project to a portfolio - private to porfolio owner POST
-    @PostMapping("/{owner}/projects")
-    public void addProjectToPortfolio(@PathVariable String owner, @RequestBody Portfolio portfolio){
-        //TODO
-        Portfolio porfolio = portfolioService.getPortfolio(owner);
-
+    //GET ALL PROJECTS
+    @GetMapping("/{portfolioName}/projects")
+    public ResponseEntity<List<Project>> getProjects(@PathVariable String portfolioName) {
+        return ResponseEntity.ok(portfolioService.getProjects(portfolioName));
     }
 
+    //GET PROJECT
+    @GetMapping("/{portfolioName}/projects/{id}")
+    public ResponseEntity<Project> getProject(@PathVariable String portfolioName, @PathVariable String id) {
+        return ResponseEntity.ok(portfolioService.getProject(portfolioName, id));
+    }
 
+    //TODO: Add security layer to the following methods
 
+    //Only admin methods
+    //CREATE PORTFOLIO
+    @PostMapping("/addPortfolio")
+    public ResponseEntity<Portfolio> addPortfolio(@RequestBody Portfolio portfolio) {
+        return ResponseEntity.ok(portfolioService.addPortfolio(portfolio));
+    }
+
+    //User methods
+    //UPDATE PORTFOLIO INFO
+    @PutMapping("/{portfolioName}")
+    public ResponseEntity<Portfolio> updatePortfolioInfo(@PathVariable String portfolioName, @RequestBody Portfolio portfolio) {
+        return ResponseEntity.ok(portfolioService.updatePortfolioInfo(portfolioName, portfolio));
+    }
+
+    //UPDATE PORTFOLIO TITLE
+    @PutMapping("/{portfolioName}/title")
+    public ResponseEntity<Portfolio> updatePortfolioTitle(@PathVariable String portfolioName, @RequestBody Title title) {
+        return ResponseEntity.ok(portfolioService.updatePortfolioTitle(portfolioName, title));
+    }
+
+    //UPDATE PORTFOLIO ABOUT
+    @PutMapping("/{portfolioName}/about")
+    public ResponseEntity<Portfolio> updatePortfolioAbout(@PathVariable String portfolioName, @RequestBody String about) {
+        return ResponseEntity.ok(portfolioService.updatePortfolioAbout(portfolioName, about));
+    }
+
+    //ADD PROJECT
+    @PostMapping("/{portfolioName}/addProject")
+    public ResponseEntity<Project> addProject(@PathVariable String portfolioName, @RequestBody Project project) {
+        return ResponseEntity.ok(portfolioService.addProject(portfolioName, project));
+    }
+
+    //UPDATE PROJECT
+    @PutMapping("/{portfolioName}/updateProject")
+    public ResponseEntity<Project> updateProject(@PathVariable String portfolioName, @RequestBody Project project) {
+        return ResponseEntity.ok(portfolioService.updateProject(portfolioName, project));
+    }
+
+    //DELETE PROJECT
+    @DeleteMapping("/{portfolioName}/deleteProject/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable String portfolioName, @PathVariable String id) {
+        portfolioService.deleteProject(portfolioName, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //ADD TAG
+    @PostMapping("/{portfolioName}/addTag")
+    public ResponseEntity<Project> addTag(@PathVariable String portfolioName, @RequestBody Project project) {
+        return ResponseEntity.ok(portfolioService.addTag(portfolioName, project));
+    }
+
+    //UPDATE TAG
+    @PutMapping("/{portfolioName}/updateTag")
+    public ResponseEntity<Project> updateTag(@PathVariable String portfolioName, @RequestBody Project project) {
+        return ResponseEntity.ok(portfolioService.updateTag(portfolioName, project));
+    }
+
+    //DELETE TAG
+    @DeleteMapping("/{portfolioName}/deleteTag/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable String portfolioName, @PathVariable String id) {
+        portfolioService.deleteTag(portfolioName, id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
